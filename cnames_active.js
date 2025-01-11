026ebb5afcbf641cb3bd467c1307dfb63df7a6d1
donist-enterprise <rayan.paille27@gmail.com>
2025-01-11 13:03:51 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3690,6 +3690,8 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "lib-donist": "donist-enterprise.github.io/lib/home"
+  "donist-enterprise": "donist-enterprise.github.io/"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
